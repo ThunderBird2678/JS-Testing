9f8970ec4dd136f9f6f978b23fd95d94bcc1bf5d
ThunderBird2678 <edwardthetrueblade2678@gmail.com>
2018-02-18 18:02:05 -0500

    THINGS WORK
    Can pull a lot of clan data now

--- fancy.js
+++ fancy.js
@@ -1,18 +1,47 @@
 var objec;
+var clanID;
 
-var input = document.getElementById("searchTxt").value;
-console.log(input);
+function search()
+{
 
-fetch('https://api.worldofwarships.com/wows/clans/list/?application_id=demo&search=DOW')
+	var input = document.getElementById("searchTxt").value;
+	var outputArea = document.getElementById("holder");
+	var outputText = "";
+	console.log(input);
+
+	fetch('https://api.worldofwarships.com/wows/clans/list/?application_id=demo&search=' + input)
 	.then(response => response.json())
-		.then(data => 
+	.then(data => 
+	{
+		objec = data;
+		//console.log(objec);
+		//console.log(typeof(objec));
+		console.log(objec.data);
+		//console.log(objec.data[0]);
+		//console.log(objec.data[0].clan_id + ", " + objec.data[0].name);
+		outputText += ("Tag: [" + objec.data[0].tag + "] <br>");
+		outputText += ("Name: " + objec.data[0].name + '<br>');
+		outputText += ("ID: " + objec.data[0].clan_id+ '<br>');
+		clanID = objec.data[0].clan_id;
+		return clanID;
+	})
+	.then(clanID =>
+	{
+		fetch('https://api.worldofwarships.com/wows/clans/info/?application_id=demo&clan_id=' + clanID)
+		.then(response => response.json())
+		.then(data =>
 		{
-			objec = data;
-			console.log(objec);
-			console.log(typeof(objec));
-			console.log(objec.data[0]);
-			console.log(objec.data[0].clan_id + ", " + objec.data[0].name);
-			document.getElementById("holder").innerHTML = objec.data[0].clan_id + ", " + objec.data[0].name;
+			var detail = data;
+			console.log(detail.data[clanID]);
+			outputText += ("Created by: " + detail.data[clanID].creator_name + "<br>");
+			outputText += ("Current Commander: " + detail.data[clanID].leader_name + "<br>");
+			outputText += ("Description: " + detail.data[clanID].description + "<br>");
+			outputArea.innerHTML = outputText;
 		})
+	})
+
+}
+
+
 
 
