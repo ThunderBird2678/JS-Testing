a39664b506b59bcf500c877e5d14b50429bbefba
Kai Huang <edwardthetrueblade2678@gmail.com>
2019-02-24 16:09:03 -0500

    Refactoring !!
    https://harusawakaito.tumblr.com/post/183012145147/refactoring-and-janky-sync

--- fancy.js
+++ fancy.js
@@ -1,104 +1,142 @@
-var clanID;
+var playersDictionary;
 
 function search()
 {
+	startUp();
+}
 
+function getAPIInfo(url)
+{
+	return fetch(url)
+		.then (info => info.json());
+}
+
+function addToPage(str)
+{
+	document.getElementById("holder").innerHTML += str;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function startUp()
+{
+	document.getElementById("holder").innerHTML = "";
 	var input = document.getElementById("searchTxt").value;
-	var outputArea = document.getElementById("holder");
-	var outputText = "";
 	console.log(input);
 
-	// Set up a variable even earlier to store the Clan ID in
 	var clanID;
 
-	fetch('https://api.worldofwarships.com/wows/clans/list/?application_id=65e92b6a331f1aa07d01dd46ed8f4821&search=' + input)
-	.then
-	(
-		function(response)
-		{
-			return response.json();
-		}
-	)
-	.then
-	(
-		function(data) 
-		{
-			// Set up a temporary object to store the JSON information in
-			var objec = data;
-			// I hate this syntax since WG used "data" as the keyword for clan info, but oh well
-			console.log(objec.data);
-			// Output the information to the screen
-			outputText += ("Tag: [" + objec.data[0].tag + "] <br>");
-			outputText += ("Name: " + objec.data[0].name + '<br>');
-			outputText += ("ID: " + objec.data[0].clan_id+ '<br>');
-			// Store the clan ID
-			clanID = objec.data[0].clan_id;
-			// And pass it forwards
-			return clanID;
-		}
-	)
-	.then
-	(
-		// After reading in the Clan ID
-		function(clanID)
-		{
-			// Fetch the clan information based on the ID given
-			fetch('https://api.worldofwarships.com/wows/clans/info/?application_id=65e92b6a331f1aa07d01dd46ed8f4821&clan_id=' + clanID)
-			.then
-			(
-				function(response)
-				{
-					// Similarly we'll take the response, store it as a JSON object, and pass it forwards
-					return response.json();
-				}
-			)
-			.then
-			(
-				function(data)
-				{
-					// We'll similarly output all of the details in the similar fashion
-					var objec = data;
-					console.log(objec.data[clanID]);
-					outputText += ("Created by: " + objec.data[clanID].creator_name + "<br>");
-					outputText += ("Current Commander: " + objec.data[clanID].leader_name + "<br>");
-					outputText += ("Description: " + objec.data[clanID].description + "<br>");
-					outputArea.innerHTML = outputText;
-					// We'll then daisy chain the clanID straight through here
-					return objec.data[clanID];
-				}
-			)
-			.then
-			(
-				function(objec)
-				{
-					console.log(objec.members_count);
-					var numMembers = objec.members_count;
-					for(var i = 0; i < numMembers; i++)
-					{
-						var playerID = objec.members_ids[i];
-						fetch("https://api.worldofwarships.com/wows/account/info/?application_id=65e92b6a331f1aa07d01dd46ed8f4821&account_id=" + playerID)
-						.then
-						(
-							function(response)
-							{
-								var block = response.json();
-								return block;
-							}
-						)
-						.then
-						(
-							function(passThrough)
-							{
-								console.log(passThrough.data[playerID]);
-							}
-						)
-					}
-				}
-			)
-		}
-	)
+	getAPIInfo('https://api.worldofwarships.com/wows/clans/list/?application_id=65e92b6a331f1aa07d01dd46ed8f4821&search=' + input)
+	.then (info => 
+	{
+		console.log(info.data);
+		addToPage("Clan Tag: [" + info.data[0].tag + "] <br>");
+		addToPage("Name: " + info.data[0].name + "<br>");
+		addToPage("Clan ID: " + info.data[0].clan_id + " <br>");
 
+		clanID = info.data[0].clan_id;
+		return clanID;
+	})
+	.then(clanID => getAPIInfo('https://api.worldofwarships.com/wows/clans/info/?application_id=65e92b6a331f1aa07d01dd46ed8f4821&clan_id=' + clanID))
+	.then(info => 
+	{
+		info = jsonPath(info.data, "$.*")[0];
+		console.log(info);
+		addToPage("Creator: " + info.creator_name + "<br>");
+		addToPage("Current Commander: " + info.leader_name + "<br>");
+		addToPage("Description: " + info.description + "<br>");
+		addToPage("Members: <br>");
+
+		var memberInfo = new Object();
+		memberInfo["members_count"] = info.members_count;
+		memberInfo["members_ids"] = info.members_ids;
+		return memberInfo;
+	})
+	.then(async memberInfo => 
+	{
+		var isDone = false;
+		playersDictionary = new Object();
+		for(var i = 0; i < memberInfo.members_count; i++)
+		{
+			var counter = 0;
+			getAPIInfo("https://api.worldofwarships.com/wows/account/info/?application_id=65e92b6a331f1aa07d01dd46ed8f4821&account_id=" + memberInfo.members_ids[i])
+			.then(info =>
+			{
+				info = jsonPath(info, "$.data.*")[0];
+				addToPage(info.nickname + "<br>");
+				playersDictionary[info.account_id] = info;
+				console.log(info);
+			})
+			.then(func => 
+			{
+				counter += 1; 
+				if(counter == memberInfo.members_count)
+				{
+					isDone = true;
+					console.log("Stop sleeping!!");
+				}
+			})
+		}
+
+		while(!isDone)
+		{
+			await sleep(500);
+		}
+	})
+	.then(test=>
+	{
+		console.log("When does this print?");
+	})
 }
+
+// 					for(var i = 0; i < numMembers; i++)
+// 					{
+// 						playersDictionary[objec.members_ids[i]] = "TEST: " + objec.members_ids[i];
+// 					}
+
+// 					for(var i = 0; i < numMembers; i++)
+// 					{
+// 						var playerID = objec.members_ids[i];
+// 						fetch("https://api.worldofwarships.com/wows/account/info/?application_id=65e92b6a331f1aa07d01dd46ed8f4821&account_id=" + playerID)
+// 						.then
+// 						(
+// 							function(response)
+// 							{
+// 								return response.json();
+// 							}
+// 						)
+// 						.then
+// 						(
+// 							function(rawObject)
+// 							{
+// 								var playerData = jsonPath(rawObject, "$.data.*");
+// 								playerData = playerData[0];
+// 								playersDictionary[playerData.account_id] = playerData;
+// 							}
+// 						)
+// 					}
+// 				}
+// 			)
+// 			.then
+// 			(
+// 				function()
+// 				{
+// 					doneSetup();
+// 				}
+// 			)
+// 		}
+// 	)
+// }
+
+// function doneSetup()
+// {
+// 	for(var key in playersDictionary)
+// 	{
+// 		console.log(key);
+// 	}
+// }
 
 
 
